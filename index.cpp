ff1408ec36a614adceca85ae52a957fc16351410
nishat-tonami <nishat.tonami121@gmail.com>
2024-05-07 23:46:03 +0600

    Subtraction added

--- index.cpp
+++ index.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int n,m;
     cin >> n >> m;  
-    cout << "Sum : "<<(n+m)<<endl;   
+    cout << "Sum : " << (n+m) <<endl;   
+    cout << "Subtraction : " << (n-m) <<endl;
     return 0;
 }
